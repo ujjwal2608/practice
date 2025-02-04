f191f5c5c4342cdda76025934f266a0ee73a6415
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2025-02-04 21:17:50 +0530

    reactnode elements

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,45 @@
-import { Image, StyleSheet, Platform } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { TimeInput, TimeInputHr, TimeInputMin, TimeInputMeridiemText, TimeInputMeridiem ,TimeInputColon} from '@/components/ui/time-input';
-import {Text} from "@/components/ui/text"
-import dayjs,{Dayjs} from "dayjs";
-import { useState } from 'react';
-export default function HomeScreen() {
-  const [time, setTime] = useState<Dayjs>()
-  console.log(time?.format("HH:mm"), "time");
+import React, { cloneElement, ReactNode, useState } from 'react'
+import { Text, View } from 'react-native'
+interface Product {
+  title: string
+}
+const products = [
+  { title: "phone" },
+  { title: "tv" },
+  { title: "radio" },
+]
+const Item = ({ title, isMark }: { title: string, isMark?: boolean }) => {
   return (
-   <SafeAreaView className="flex flex-1 flex-col gap-4 p-4 items-center justify-center">
-    <Text className="text-red-500 font-bold">Hello</Text>
-    
-   </SafeAreaView>
+    <View className={`px-3 py-2 border rounded-lg ${isMark ? 'bg-red-400 text-white' : ''}`}>
+      <Text className='text-2xl'>{title}</Text>
+    </View>
+  )
+}
+const List = ({ children }: { children: ReactNode }) => {
+  const [selectedIndex, setSelectedindex] = useState<number>(1)
+  console.log(selectedIndex)
+  return (
+    <View className='gap-2'>
+      {React.Children.map(children,(child, index) =>
+        cloneElement(child as React.ReactElement, {
+          isMark: index === selectedIndex,  // Now index is properly mapped
+        })
+      )}
+    </View>
+  )
+}
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className="flex flex-1 flex-col gap-4 p-4 items-center justify-center">
+      <View>
+        <List>
+          {products.map(product => <Item title={product.title} key={product.title} />)}
+        </List>
+      </View>
+
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
