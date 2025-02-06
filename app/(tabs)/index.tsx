32f5dfe72efd79f061af2ca7da1b219c305f645c
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2025-02-06 23:45:37 +0530

    passing multiple props

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,13 +5,15 @@ import { Text, View } from 'react-native'
 const ParentComponent = ({ children }: { children: ReactElement }) => {
   return (
     //this can only clone one element thats why i passed the ReactElement type not the react node
-    cloneElement(children, { color: "red" }, null)
+    cloneElement(children, { color: "red",color2:"blue",color3:"green" },null)
   )
 }
-const ChildComponent = ({ color }: { color?: string }) => {
+const ChildComponent = ({ color,color2,color3 }: { color?: string,color2?:string,color3?:string }) => {
   return (
     <View>
       <Text style={{ fontSize: 24, color: color }}>child component</Text>
+      <Text style={{ fontSize: 24, color: color2 }}>child component</Text>
+      <Text style={{ fontSize: 24, color: color3 }}>child component</Text>
     </View>
   )
 }
