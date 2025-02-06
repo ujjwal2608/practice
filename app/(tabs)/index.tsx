1ae2de39a9f975e031cad46f2194395533180884
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2025-02-06 23:39:56 +0530

    clone element with react element (single element)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,45 +1,30 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
-import React, { cloneElement, ReactNode, useState } from 'react'
+import React, { cloneElement, ReactElement, ReactNode, useState } from 'react'
 import { Text, View } from 'react-native'
-interface Product {
-  title: string
-}
-const products = [
-  { title: "phone" },
-  { title: "tv" },
-  { title: "radio" },
-]
-const Item = ({ title, isMark }: { title: string, isMark?: boolean }) => {
+
+const ParentComponent = ({ children }: { children: ReactElement }) => {
   return (
-    <View className={`px-3 py-2 border rounded-lg ${isMark ? 'bg-red-400 text-white' : ''}`}>
-      <Text className='text-2xl'>{title}</Text>
+    //this can only clone one element thats why i passed the ReactElement type not the react node
+    cloneElement(children, { color: "red" }, null)
+  )
+}
+const ChildComponent = ({ color }: { color?: string }) => {
+  return (
+    <View>
+      <Text style={{ fontSize: 24, color: color }}>child component</Text>
     </View>
   )
 }
-const List = ({ children }: { children: ReactNode }) => {
-  const [selectedIndex, setSelectedindex] = useState<number>(1)
-  console.log(selectedIndex)
+export default function app() {
   return (
-    <View className='gap-2'>
-      {React.Children.map(children,(child, index) =>
-        cloneElement(child as React.ReactElement, {
-          isMark: index === selectedIndex,  // Now index is properly mapped
-        })
-      )}
-    </View>
-  )
-}
-
-export default function HomeScreen() {
-  return (
-    <SafeAreaView className="flex flex-1 flex-col gap-4 p-4 items-center justify-center">
-      <View>
-        <List>
-          {products.map(product => <Item title={product.title} key={product.title} />)}
-        </List>
-      </View>
-
+    <SafeAreaView>
+      <Text>befor wrapper</Text>
+      <ChildComponent />
+      <Text>after the wrapper</Text>
+      <ParentComponent>
+        <ChildComponent />
+      </ParentComponent>
     </SafeAreaView>
-  );
-}
+  )
 
+}
