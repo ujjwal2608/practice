5342bf3a22d7f631749a1b7d2da87bac29f84cf8
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2025-02-07 00:12:34 +0530

    pass the children on clone lement to the child

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,24 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
-import React, { cloneElement, ReactElement, ReactNode, useState } from 'react'
+import React, { cloneElement, ReactElement, ReactNode, useState,Children } from 'react'
 import { Text, View } from 'react-native'
 
 const ParentComponent = ({ children }: { children: ReactElement }) => {
   return (
     //this can only clone one element thats why i passed the ReactElement type not the react node
-    cloneElement(children, { color: "red",color2:"blue",color3:"green" },null)
+    cloneElement(children, { color1:"red" },<Text>replace</Text>)
   )
 }
-const ChildComponent = ({ color,color2,color3 }: { color?: string,color2?:string,color3?:string }) => {
+const ChildComponent = ({ color,color2,color3,children }: { color?: string,color2?:string,color3?:string,children?:ReactElement }) => {
   return (
     <View>
       <Text style={{ fontSize: 24, color: color }}>child component</Text>
       <Text style={{ fontSize: 24, color: color2 }}>child component</Text>
       <Text style={{ fontSize: 24, color: color3 }}>child component</Text>
+      {children}
     </View>
   )
 }
+
 export default function app() {
   return (
     <SafeAreaView>
