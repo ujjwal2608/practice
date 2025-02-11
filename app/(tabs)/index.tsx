b4ce40ee7bbed2b08283feaee0db17555ca9e1e7
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2025-02-12 00:37:25 +0530

    eg for use call abck and use memo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,43 @@
-import { SafeAreaView } from 'react-native-safe-area-context';
-import React, { cloneElement, ReactElement, ReactNode, useState,Children } from 'react'
-import { Text, View } from 'react-native'
+import { memo, useCallback, useState } from "react";
+import { View, Text, Button } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const ParentComponent = ({ children }: { children: ReactElement }) => {
-  return (
-    //this can only clone one element thats why i passed the ReactElement type not the react node
-    cloneElement(children, { color1:"red" },<Text>replace</Text>)
-  )
-}
-const ChildComponent = ({ color,color2,color3,children }: { color?: string,color2?:string,color3?:string,children?:ReactElement }) => {
+const listdata = [
+  { name: "Ujjwal", id: "1" },
+  { name: "Ujjwal1", id: "2" },
+  { name: "Ujjwal2", id: "3" },
+  { name: "Ujjwal3", id: "4" },
+  { name: "Ujjwal4", id: "5" },
+  { name: "Ujjwal5", id: "6" }
+]
+
+
+const Card = memo(({ listItem, handleDelete }: { listItem: { name: string; id: string }; handleDelete: (id: string) => void }) => {
+  console.log("render", listItem.id)
   return (
     <View>
-      <Text style={{ fontSize: 24, color: color }}>child component</Text>
-      <Text style={{ fontSize: 24, color: color2 }}>child component</Text>
-      <Text style={{ fontSize: 24, color: color3 }}>child component</Text>
-      {children}
+      <Text className="text-2xl">{listItem.name}</Text>
     </View>
   )
-}
-
-export default function app() {
+})
+export default function App() {
+  const [list, setList] = useState(listdata)
+  const handleDelete = (id: string) => {
+    setList((prev) => prev.filter((item) => item.id !== id));
+  }
   return (
-    <SafeAreaView>
-      <Text>befor wrapper</Text>
-      <ChildComponent />
-      <Text>after the wrapper</Text>
-      <ParentComponent>
-        <ChildComponent />
-      </ParentComponent>
+    <SafeAreaView className="flex-1">
+      <View>
+        {list.map((listItem) => (
+          <Card key={listItem.id} listItem={listItem} handleDelete={handleDelete} />//it should use both use callback and use memo it we are passinfg the both value and 
+        ))}
+        <Button title="delete1" onPress={() => { handleDelete("1") }} />
+        <Button title="delete2" onPress={() => { handleDelete("2") }} />
+        <Button title="delete3" onPress={() => { handleDelete("3") }} />
+        <Button title="delete4" onPress={() => { handleDelete("4") }} />
+        <Button title="delete5" onPress={() => { handleDelete("5") }} />
+        <Button title="delete6" onPress={() => { handleDelete("6") }} />
+      </View>
     </SafeAreaView>
   )
-
 }
